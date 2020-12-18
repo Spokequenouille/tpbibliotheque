e2fa595c59ff8969bb249289a829fc1080b49cc4
spokequenouille <spokequenouille@gmail.com>
2020-12-18 18:20:15 +0100

    postman

--- src/main/java/spkqnllgang/bibliotheque/Repository/CommentaireRepository.java
+++ src/main/java/spkqnllgang/bibliotheque/Repository/CommentaireRepository.java
@@ -1,5 +1,7 @@
 package spkqnllgang.bibliotheque.Repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,4 +10,5 @@ import spkqnllgang.bibliotheque.Entity.Commentaire;
 @RepositoryRestResource(path="commentaire")
 public interface CommentaireRepository extends JpaRepository<Commentaire, Integer>{
     void deleteCommentaireById(int id);
+    Set<Commentaire> findCommentaireByText(String text);
 }
